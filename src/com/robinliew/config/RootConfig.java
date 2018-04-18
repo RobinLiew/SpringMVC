ec20882e824eec22f5b373f0a8041f1020c101fe
RobinLiew <1498975990@qq.com>
2018-04-18 11:49:26 +0800

    Update RootConfig.java

--- src/com/robinliew/config/RootConfig.java
+++ src/com/robinliew/config/RootConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+/**
+ * 该配置类用来配置ContextLoaderListener创建的应用上下文中的bean，
+ * 这些bean通常是驱动应用后端的中间层和数据层组件。
+ * 
+ */
 @Configuration
 @ComponentScan(basePackages={"com.robinliew"},excludeFilters={@Filter(type=FilterType.ANNOTATION,value=EnableWebMvc.class)})
 public class RootConfig {
